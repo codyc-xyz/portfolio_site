25a7774636da5bd2c4605cf49705092fb602d104
Red <crabmarket56@gmail.com>
2023-06-03 21:25:12 +0900

    refactor: refactor music player and remove gifs

--- src/components/general/MusicPlayer.tsx
+++ src/components/general/MusicPlayer.tsx
@@ -1,22 +1,82 @@
 import React, { useState, useEffect } from 'react';
 import Song from '../../types/Song';
 
-interface MusicPlayerProps {
-  songs: Song[];
-  className?: string;
-}
+type ButtonSelection = 'starryNight' | 'discoBall';
 
-const MusicPlayer: React.FC<MusicPlayerProps> = ({ songs, className }) => {
+const MusicPlayer: React.FC = () => {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const audioRef = React.useRef(new Audio(songs[currentSongIndex].audioSrc));
+  const dreamsPlaylist: Song[] = [
+    {
+      title: `Dream a Little Dream of Me`,
+      artist: `Dean Martin`,
+      audioSrc: `/static/audio/Dream_a_Little_Dream_of_Me.mp3`,
+    },
+    {
+      title: `Fruit of Dreams`,
+      artist: `Les Baxter`,
+      audioSrc: `/static/audio/Fruit_Of_Dreams.mp3`,
+    },
+    {
+      title: `Look To The Sky`,
+      artist: `Antônio Carlos Jobim`,
+      audioSrc: `/static/audio/Look_To_The_Sky.mp3`,
+    },
+    {
+      title: `Tenderly`,
+      artist: `Les Baxter`,
+      audioSrc: `/static/audio/Tenderly.mp3`,
+    },
+    {
+      title: `Here's To Life`,
+      artist: `Shirley Horn`,
+      audioSrc: `/static/audio/Here's_To_Life.mp3`,
+    },
+  ];
+
+  const boogiePlaylist: Song[] = [
+    {
+      title: `Looking Up to You`,
+      artist: `Michael Wycoff`,
+      audioSrc: `/static/audio/Looking_Up_To_You.mp3`,
+    },
+    {
+      title: `Ha Ha, Hee Hee`,
+      artist: `Sly & The Family Stone`,
+      audioSrc: `/static/audio/Ha_Ha_Hee_Hee.mp3`,
+    },
+    {
+      title: `Let Love Enter`,
+      artist: `Michael Henderson`,
+      audioSrc: `/static/audio/Let_Love_Enter.mp3`,
+    },
+    {
+      title: `Mr Magician`,
+      artist: `Mystic Merlin`,
+      audioSrc: `/static/audio/Mr_Magician.mp3`,
+    },
+    {
+      title: `I'll Never Forget (My Favorite Disco)`,
+      artist: `Dexter Wansel, The Jones Girls`,
+      audioSrc: `/static/audio/I'll_Never_Forget.mp3`,
+    },
+  ];
+
+  const [currentPlaylist, setCurrentPlaylist] =
+    useState<Song[]>(dreamsPlaylist);
+  const [selectedButton, setSelectedButton] =
+    useState<ButtonSelection>(`starryNight`);
+
+  const audioRef = React.useRef(
+    new Audio(currentPlaylist[currentSongIndex].audioSrc),
+  );
 
   useEffect(() => {
-    if (currentSongIndex > songs.length) {
+    if (currentSongIndex > currentPlaylist.length) {
       setCurrentSongIndex(0);
     }
-    audioRef.current.src = songs[currentSongIndex].audioSrc;
+    audioRef.current.src = currentPlaylist[currentSongIndex].audioSrc;
 
     if (isPlaying) {
       audioRef.current.play();
@@ -24,43 +84,51 @@ const MusicPlayer: React.FC<MusicPlayerProps> = ({ songs, className }) => {
       audioRef.current.pause();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentSongIndex, isPlaying]);
+  }, [currentSongIndex, isPlaying, currentPlaylist]);
 
   const handleNext = () => {
-    setCurrentSongIndex((currentSongIndex + 1) % songs.length);
+    setCurrentSongIndex((currentSongIndex + 1) % currentPlaylist.length);
   };
 
   const handlePrev = () => {
-    setCurrentSongIndex((currentSongIndex - 1 + songs.length) % songs.length);
+    setCurrentSongIndex(
+      (currentSongIndex - 1 + currentPlaylist.length) % currentPlaylist.length,
+    );
   };
 
   const handlePlayPause = () => {
     setIsPlaying(!isPlaying);
   };
 
+  const handleButtonSelect = (playlist: Song[], button: ButtonSelection) => {
+    setCurrentPlaylist(playlist);
+    setSelectedButton(button);
+    setCurrentSongIndex(0);
+    setIsPlaying(false);
+  };
+
   return (
-    <div className={className}>
-      <button className="p-2 text-text hover:opacity-50" onClick={handlePrev}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          height="24"
-          width="24"
+    <div className="absolute right-2 bottom-2 flex flex-col space-y-2">
+      <div className="flex-row text-center">
+        <button
+          className={`${
+            selectedButton === `starryNight` ? `bg-primary` : ``
+          } text-xl p-1 m-1`}
+          onClick={() => handleButtonSelect(dreamsPlaylist, `starryNight`)}
         >
-          <polygon points="11 19 2 12 11 5 11 19"></polygon>
-          <polygon points="22 19 13 12 22 5 22 19"></polygon>
-        </svg>
-      </button>
-      <button
-        className="p-2 text-primary hover:opacity-50"
-        onClick={handlePlayPause}
-      >
-        {isPlaying ? (
+          🌌
+        </button>
+        <button
+          className={`${
+            selectedButton === `discoBall` ? `bg-primary` : ``
+          } text-xl p-1 m-1`}
+          onClick={() => handleButtonSelect(boogiePlaylist, `discoBall`)}
+        >
+          🪩
+        </button>
+      </div>
+      <div className="flex-row">
+        <button className="p-2 text-text hover:opacity-50" onClick={handlePrev}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -72,10 +140,46 @@ const MusicPlayer: React.FC<MusicPlayerProps> = ({ songs, className }) => {
             height="24"
             width="24"
           >
-            <rect x="6" y="4" width="4" height="16"></rect>
-            <rect x="14" y="4" width="4" height="16"></rect>
+            <polygon points="11 19 2 12 11 5 11 19"></polygon>
+            <polygon points="22 19 13 12 22 5 22 19"></polygon>
           </svg>
-        ) : (
+        </button>
+        <button
+          className="p-2 text-primary hover:opacity-50"
+          onClick={handlePlayPause}
+        >
+          {isPlaying ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              height="24"
+              width="24"
+            >
+              <rect x="6" y="4" width="4" height="16"></rect>
+              <rect x="14" y="4" width="4" height="16"></rect>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              height="24"
+              width="24"
+            >
+              <polygon points="5 3 19 12 5 21 5 3"></polygon>
+            </svg>
+          )}
+        </button>
+        <button className="p-2 text-text hover:opacity-50" onClick={handleNext}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -87,26 +191,11 @@ const MusicPlayer: React.FC<MusicPlayerProps> = ({ songs, className }) => {
             height="24"
             width="24"
           >
-            <polygon points="5 3 19 12 5 21 5 3"></polygon>
+            <polygon points="13 19 22 12 13 5 13 19"></polygon>
+            <polygon points="2 19 11 12 2 5 2 19"></polygon>
           </svg>
-        )}
-      </button>
-      <button className="p-2 text-text hover:opacity-50" onClick={handleNext}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          height="24"
-          width="24"
-        >
-          <polygon points="13 19 22 12 13 5 13 19"></polygon>
-          <polygon points="2 19 11 12 2 5 2 19"></polygon>
-        </svg>
-      </button>
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -1,155 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../components/general/Header';
-import homerSimpsonGif from '../../public/static/homer-simpson-comfy.gif';
-import homerMargeGif from '../../public/static/homer-marge-dancing.gif';
-import homerSimpsonStill from '../../public/static/homer-simpson-comfy-still.jpg';
-import homerMargeStill from '../../public/static/homerMargeStill.jpg';
-import Song from '../types/Song';
 import MusicPlayer from '../components/general/MusicPlayer';
 
 const FrontPage: React.FC = () => {
-  const [selectedGif, setSelectedGif] = useState<string>(``);
-  const [homerSimpsonSrc, setHomerSimpsonSrc] =
-    useState<string>(homerSimpsonStill);
-  const [homerMargeSrc, setHomerMargeSrc] = useState<string>(homerMargeStill);
-
-  const handleMouseEnter = (gifName: string) => {
-    if (gifName === `homerSimpsonGif`) {
-      setHomerSimpsonSrc(homerSimpsonGif);
-    } else if (gifName === `homerMargeGif`) {
-      setHomerMargeSrc(homerMargeGif);
-    }
-  };
-
-  const handleMouseLeave = (gifName: string) => {
-    if (gifName === `homerSimpsonGif` && selectedGif !== `homerSimpsonGif`) {
-      setHomerSimpsonSrc(homerSimpsonStill);
-    } else if (gifName === `homerMargeGif` && selectedGif !== `homerMargeGif`) {
-      setHomerMargeSrc(homerMargeStill);
-    }
-  };
-
-  const handleClick = (gifName: string) => {
-    if (selectedGif !== gifName) {
-      setSelectedGif(gifName);
-      handleMouseEnter(gifName);
-    } else {
-      setSelectedGif(``);
-      handleMouseLeave(gifName);
-    }
-  };
-
-  const dreamsPlaylist: Song[] = [
-    {
-      title: `Dream a Little Dream of Me`,
-      artist: `Dean Martin`,
-      audioSrc: `/static/audio/Dream_a_Little_Dream_of_Me.mp3`,
-    },
-    {
-      title: `Fruit of Dreams`,
-      artist: `Les Baxter`,
-      audioSrc: `/static/audio/Fruit_Of_Dreams.mp3`,
-    },
-    {
-      title: `Look To The Sky`,
-      artist: `Antônio Carlos Jobim`,
-      audioSrc: `/static/audio/Look_To_The_Sky.mp3`,
-    },
-    {
-      title: `Tenderly`,
-      artist: `Les Baxter`,
-      audioSrc: `/static/audio/Tenderly.mp3`,
-    },
-    {
-      title: `Here's To Life`,
-      artist: `Shirley Horn`,
-      audioSrc: `/static/audio/Here's_To_Life.mp3`,
-    },
-  ];
-
-  const boogiePlaylist: Song[] = [
-    {
-      title: `Looking Up to You`,
-      artist: `Michael Wycoff`,
-      audioSrc: `/static/audio/Looking_Up_To_You.mp3`,
-    },
-    {
-      title: `Ha Ha, Hee Hee`,
-      artist: `Sly & The Family Stone`,
-      audioSrc: `/static/audio/Ha_Ha_Hee_Hee.mp3`,
-    },
-    {
-      title: `Let Love Enter`,
-      artist: `Michael Henderson`,
-      audioSrc: `/static/audio/Let_Love_Enter.mp3`,
-    },
-    {
-      title: `Mr Magician`,
-      artist: `Mystic Merlin`,
-      audioSrc: `/static/audio/Mr_Magician.mp3`,
-    },
-    {
-      title: `I'll Never Forget (My Favorite Disco)`,
-      artist: `Dexter Wansel, The Jones Girls`,
-      audioSrc: `/static/audio/I'll_Never_Forget.mp3`,
-    },
-  ];
-
-  const gifStyles = (gifName: string) => {
-    if (selectedGif === gifName) {
-      return `w-13 h-13 justify-center mt-7`;
-    } else {
-      return `h-11 w-12 my-2 shadow-lg hover:shadow-sm`;
-    }
-  };
-
   return (
     <div className="text-text">
       <Header />
       <div className="flex">
-        <div className="w-1/2 flex flex-col justify-center items-start text-3xl mx-8">
+        <div className="h-full flex flex-col justify-center text-3xl m-4">
           <p className="my-2">Hi.</p>
           <p className="my-2">My name is Cody.</p>
+          <p className="my-2">
+            I&apos;m a full stack developer passionate about movies, music,
+            philosophy, and code.
+          </p>
           <p className="my-2">Welcome to my portfolio.</p>
-          <p className="my-2">Select a mood and explore!</p>
         </div>
-        <div className="w-1/2">
-          <div className="flex items-center justify-end mt-4">
-            <div className="flex flex-col justify-center">
-              {` `}
-              {selectedGif !== `homerMargeGif` && (
-                <button
-                  onClick={() => handleClick(`homerSimpsonGif`)}
-                  onMouseEnter={() => handleMouseEnter(`homerSimpsonGif`)}
-                  onMouseLeave={() => handleMouseLeave(`homerSimpsonGif`)}
-                >
-                  <img
-                    src={homerSimpsonSrc}
-                    className={gifStyles(`homerSimpsonGif`)}
-                  />
-                </button>
-              )}
-              {selectedGif === `homerSimpsonGif` && (
-                <MusicPlayer songs={dreamsPlaylist} className="mx-auto" />
-              )}
-              {selectedGif !== `homerSimpsonGif` && (
-                <button
-                  onClick={() => handleClick(`homerMargeGif`)}
-                  onMouseEnter={() => handleMouseEnter(`homerMargeGif`)}
-                  onMouseLeave={() => handleMouseLeave(`homerMargeGif`)}
-                >
-                  <img
-                    src={homerMargeSrc}
-                    className={gifStyles(`homerMargeGif`)}
-                  />
-                </button>
-              )}
-              {selectedGif === `homerMargeGif` && (
-                <MusicPlayer songs={boogiePlaylist} className="mx-auto" />
-              )}
-            </div>
-          </div>
-        </div>
+        <MusicPlayer />
       </div>
     </div>
   );
