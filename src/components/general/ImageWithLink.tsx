f25fc1d3f67fc1965ec87dd809e8fed164e31fde
Red <crabmarket56@gmail.com>
2023-06-16 20:54:16 +0900

    style: fix misc dynamic style issues

--- src/components/general/ImageWithLink.tsx
+++ src/components/general/ImageWithLink.tsx
@@ -22,7 +22,7 @@ export const ImageWithLink: React.FC<ImageWithLinkProps> = ({
   return (
     <div className="flex flex-col items-center m-1 lg:mb-4 bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-sm">
       <Link to={link} className="relative">
-        <div className="md:w-9 md:h-10.5 lg:w-10.5 lg:h-11.5 flex justify-center items-center">
+        <div className="flex justify-center items-center h-10.5 sm:h-11.5 md:h-10 lg:h-10.5">
           {isLoading && <Spinner />}
           <img
             className="w-full h-full"
