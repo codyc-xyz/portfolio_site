53bee18df8528d5575e6eaf909912747bd43b4e8
Red <crabmarket56@gmail.com>
2023-06-18 13:28:30 +0900

    style: fix title positioning on lg screen directors/authors

--- src/components/general/TitleComponent.tsx
+++ src/components/general/TitleComponent.tsx
@@ -8,7 +8,7 @@ interface TitleComponentProps {
 const TitleComponent: React.FC<TitleComponentProps> = ({ text, className }) => {
   return (
     <h1
-      className={`${className} w-full text-center text-xl md:text-lg lg:text-xl xl:text-2xl flex-grow flex-shrink-0`}
+      className={`${className} w-full text-center text-xl md:text-lg lg:text-xl xl:text-2xl lg:w-2/5 flex-grow flex-shrink-0`}
     >
       {text}
     </h1>
