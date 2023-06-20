c587ef126859d2cfe6768b5d27957afcfa68044f
Red <crabmarket56@gmail.com>
2023-06-20 16:35:49 +0900

    fix: update routing to use gatsby

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,6 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import FrontPage from './FrontPage';
-import Movies from './movies';
+import { Link } from 'gatsby';
 import '../styles/global.css';
-import Directors from './directors';
-import Books from './books';
-import Authors from './authors';
-import Projects from './projects';
 
 export const client = new ApolloClient({
   uri: `${process.env.GATSBY_SERVER_URL}/graphql`,
@@ -16,16 +10,14 @@ export const client = new ApolloClient({
 export default function Home() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<FrontPage />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/directors" element={<Directors />} />
-          <Route path="/books" element={<Books />} />
-          <Route path="/authors" element={<Authors />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </Router>
+      <div>
+        <Link to="/">FrontPage</Link>
+        <Link to="/movies">Movies</Link>
+        <Link to="/directors">Directors</Link>
+        <Link to="/books">Books</Link>
+        <Link to="/authors">Authors</Link>
+        <Link to="/projects">Projects</Link>
+      </div>
     </ApolloProvider>
   );
 }
