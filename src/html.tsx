41d6953846ba9fbbf60ae256503d480e30b8a9ce
Red <crabmarket56@gmail.com>
2023-06-24 09:51:05 +0900

    style: update favicon

--- src/html.tsx
+++ src/html.tsx
@@ -19,7 +19,7 @@ const HTML: React.FC<HTMLProps> = (props) => {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
