48c5e0d99f738311d4136e106739054295dc7c31
Red <crabmarket56@gmail.com>
2023-06-24 13:08:25 +0900

    fix: fix favicon path

---
+++ src/html.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+interface HTMLProps {
+  htmlAttributes: React.HTMLAttributes<HTMLHtmlElement>;
+  headComponents: React.ReactNode[];
+  bodyAttributes: React.HTMLAttributes<HTMLBodyElement>;
+  preBodyComponents: React.ReactNode[];
+  body: string;
+  postBodyComponents: React.ReactNode[];
+}
+
+const HTML: React.FC<HTMLProps> = (props) => {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+};
+
+export default HTML;
